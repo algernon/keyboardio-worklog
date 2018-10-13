25515d238b53a262ba0855b675a655c64d3841d9
Gergely Nagy <algernon@keyboard.io>
2018-10-13 12:27:04 +0200

    monorepo-prep: Better include massaging
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- tools/helpers/add-plugin-namespace.pl
+++ tools/helpers/add-plugin-namespace.pl
@@ -7,14 +7,18 @@ $/ = undef;
 my $data = <$fh>;
 close $fh;
 
+# Includes
+$data =~ s/^#include <Kaleidoscope\//#include <kaleidoscope\//mg;
+$data =~ s,^#include <kaleidoscope/([^/]+)\.h>,#include <kaleidoscope/plugin/$1.h>,mg;
+
 ## plugin namespace
 $data =~ s#^namespace kaleidoscope \{#namespace kaleidoscope {\nnamespace plugin {#m;
 ## close the namespace
 $data =~ s#^\};\n\};?\n#};\n}\n}\n#m;
 $data =~ s#^\}\n\s+};?\n#}\n\n}\n}\n#m;
 ## singleton
-$data =~ s#^(extern kaleidoscope::)#$1plugin::#m;
-$data =~ s#^kaleidoscope::([^\s]+) #kaleidoscope::plugin::$1 #m;
+$data =~ s#^(extern kaleidoscope::)#$1plugin::#mg;
+$data =~ s#^kaleidoscope::([^\s]+) #kaleidoscope::plugin::$1 #mg;
 
 open my $ofh, '>', $ARGV[0] or die;
 print $ofh $data;
