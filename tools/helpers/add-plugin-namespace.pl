39c50296e51d9861966c74e8c49ad88cb2da2429
Gergely Nagy <algernon@keyboard.io>
2018-10-13 11:24:52 +0200

    Add a monorepo migration tool
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ tools/helpers/add-plugin-namespace.pl
@@ -0,0 +1,18 @@
+#! /usr/bin/perl -w
+use warnings;
+use strict;
+
+open my $fh, '<', $ARGV[0] or die;
+$/ = undef;
+my $data = <$fh>;
+close $fh;
+
+$data =~ s#^namespace kaleidoscope \{#namespace kaleidoscope {\nnamespace plugin {#m;
+$data =~ s#^\};\n\}\n#};\n}\n}\n#m;
+$data =~ s#^\}\n\s+}\n#}\n\n}\n}\n#m;
+$data =~ s#^(extern kaleidoscope::)#$1plugin::#m;
+$data =~ s#^kaleidoscope::([^\s]+) #kaleidoscope::plugin::$1 #m;
+
+open my $ofh, '>', $ARGV[0] or die;
+print $ofh $data;
+close $ofh;
