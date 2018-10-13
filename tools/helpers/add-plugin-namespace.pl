bff448ba3e40735595eeadc35931053ac0fc5006
Gergely Nagy <algernon@keyboard.io>
2018-10-13 15:08:03 +0200

    tools/monrepo-prep: Assorted fixes
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- tools/helpers/add-plugin-namespace.pl
+++ tools/helpers/add-plugin-namespace.pl
@@ -7,14 +7,15 @@ $/ = undef;
 my $data = <$fh>;
 close $fh;
 
-# Includes
+## includes
 $data =~ s/^#include <Kaleidoscope\//#include <kaleidoscope\//mg;
 $data =~ s,^#include <kaleidoscope/([^/]+)\.h>,#include <kaleidoscope/plugin/$1.h>,mg;
-
+## fix kaleidoscope/hid.h includes
+$data =~ s,^#include <kaleidoscope/plugin/hid.h>,#include <kaleidoscope/hid.h>,mg;
 ## plugin namespace
 $data =~ s#^namespace kaleidoscope \{#namespace kaleidoscope {\nnamespace plugin {#m;
 ## close the namespace
-$data =~ s#^\};\n\};?\n#};\n}\n}\n#m;
+$data =~ s#^\};\n+\};?\n#};\n}\n}\n#m;
 $data =~ s#^\}\n\s+};?\n#}\n\n}\n}\n#m;
 ## singleton
 $data =~ s#^(extern kaleidoscope::)#$1plugin::#mg;
