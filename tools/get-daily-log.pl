6f84529655e8443f23f7497004e189550acd2944
Gergely Nagy <algernon@keyboard.io>
2019-02-22 10:15:09 +0100

    Add a daily log mailer
    Fixes #2.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ tools/get-daily-log.pl
@@ -0,0 +1,55 @@
+#! /usr/bin/perl -w
+use strict;
+use warnings;
+use DateTime;
+
+my $now = DateTime->now;
+$now->set( day => $now->day - 1);
+my $date = $ARGV[0] || $now->ymd;
+
+my $extracting = 0;
+
+open(my $fh, "<", "worklog.md") or die "Error opening worklog.md: $!";
+
+my @lines = <$fh>;
+my @log = ();
+my @links = ();
+
+my $header = <<"END_MESSAGE";
+From: Gergely Nagy <algernon\@keyboard.io>
+To: Jesse Vincent <jesse\@keyboard.io>
+Cc: Gergely Nagy <algernon\@keyboard.io>
+Subject: algernon's worklog for $date
+User-Agent: get-daily-log.pl Perl/$]
+
+END_MESSAGE
+
+for (@lines) {
+    if ($extracting && /^# /) {
+        $extracting = 0;
+    }
+    if (/^# $date/) {
+        $extracting = 1;
+    }
+    if ($extracting && /\[[^\]]*\]\[([^\/]*)\/([^\]]*)\]/) {
+        push @links, qr/^\s+\[$1\/$2\]:/;
+    }
+    next if ($extracting && /^ \[[^:]*\]:/);
+    push @log, $_ if $extracting;
+}
+
+exit(0) if $#log < 0;
+
+chop $log[-1];
+
+print $header;
+
+for (@log) {
+    print;
+}
+
+for my $line (@lines) {
+    for (@links) {
+        print $line if $line =~ $_;
+    }
+}
